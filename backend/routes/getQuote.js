eaa20eb762575e0d2e0c9b0faa4fe6cf5e316390
Sorin Chirtoaca <129075840+fikesoft@users.noreply.github.com>
2025-01-12 22:32:46 +0200

    Prep deploy

--- backend/routes/getQuote.js
+++ backend/routes/getQuote.js
@@ -1,9 +1,8 @@
 import express from 'express';
 import {getTodaysQuote} from '../controllers/quoteController.js'
-import {authMiddleware} from '../middlewares/authMiddlewere.js'
 const router = express.Router();
 
 // Register user route with middleware
-router.get('/',authMiddleware,getTodaysQuote);
+router.get('/',getTodaysQuote);
 
 export default router;
